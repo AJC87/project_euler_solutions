c2b3cca15fc12d85a5d9ed87ba18fa42b55d4d4a
Ashley Craig <ashleycraig1987@gmail.com>
2023-09-22 22:48:42 +0000

    Tidy up

--- problems/5/c/solution.c
+++ problems/5/c/solution.c
@@ -3,10 +3,6 @@
 int main(void) {
     int start = 20;
     int i;
-    int count = 0;
-    int iterations = 0;
-    
-    // 14 - 7, 16 - 8, 18 - 9
     
     for (i = start; ; i += 20) {
         if ((i % 19 != 0) || (i % 17 != 0) || (i % 13 != 0) || (i % 11 != 0) || (i % 18 != 0) || (i % 16 != 0) || (i % 14 != 0)) {
